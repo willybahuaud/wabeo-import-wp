2852d7d4e9d2ecd236c4acf815ce9a41e406626e
Willy Bahuaud <willy@wabeo.fr>
2013-05-09 23:31:30 +0200

    new tasks

--- import-wp.php
+++ import-wp.php
@@ -10,12 +10,46 @@ function import( $args ) {
     extract( $args );
 
     foreach( $authors as $author ) {
+        /**
+        Author exist ?
+        * is an existing email, ID
+
+        */
+
         //
     }
 
     foreach( $attachments as $attachment ) {
         //
+        /**
+        Get image
+        */
+
+        /**
+        Insert image
+        */
     }
+
+
+    /**
+    DELTA BEFORE INSERTING POST
+    */
+
+    /**
+    Have to update ?
+    */
+
+    /**
+    Have to catch ghosts ?
+    */
+
+    /**
+    Have to delete ghosts ?
+    */
+
+    /**
+    Have to group for cron task ?
+    */
 
     foreach( $posts as $post ) {
         /**
@@ -55,6 +89,10 @@ function import( $args ) {
                 }
                 /**
                 Link image
+                */
+
+                /**
+                Is post thumbnail ?
                 */
             }
             foreach( $metas as $meta ) {
